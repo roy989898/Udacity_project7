28b981f99c4e6b8bcbd4a2347af670bf66c13298
roy989898 <roy989898@gmail.com>
2016-06-20 13:55:07 +0800

    feat: add the Name and notification setting to the Seeting page
    add at the preferences.xml

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/MainActivity.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/MainActivity.java
@@ -1,22 +1,21 @@
 package pom.poly.com.tabatatimer;
 
-import android.support.design.widget.TabLayout;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -82,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent SeetingIntent = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(SeetingIntent);
             return true;
         }
 
