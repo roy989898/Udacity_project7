f51b6bc7322d21ecbe8026c851efce8bd5aa9399
roy989898 <roy989898@gmail.com>
2016-06-21 12:08:07 +0800

    feat: add a Circle image view and can click(the click action haven't implement)

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/profilePictureSetingActivity.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/profilePictureSetingActivity.java
@@ -1,13 +1,28 @@
 package pom.poly.com.tabatatimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class profilePictureSetingActivity extends AppCompatActivity {
+
+    @BindView(R.id.imProfile)
+    CircleImageView imProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_picture_seting);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.imProfile)
+    public void onClick() {
+        Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
     }
 }
