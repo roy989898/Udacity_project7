d4aaca109d72f3a81a3c3df55f318a62c8f9665f
roy989898 <roy989898@gmail.com>
2016-06-22 20:25:03 +0800

    feat:in the setting activity,the preference can use the summery to show the value

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/SeetingFragment.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/SeetingFragment.java
@@ -1,15 +1,39 @@
 package pom.poly.com.tabatatimer.Fragment;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import pom.poly.com.tabatatimer.R;
 
 
-public class SeetingFragment extends PreferenceFragment {
+public class SeetingFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference namePreference = findPreference(getString(R.string.preference_name_key));
+        Preference notificationtimePreference = findPreference(getString(R.string.preference_timePick_key));
+
+        bindPreferenceChangeListener(namePreference);
+        bindPreferenceChangeListener(notificationtimePreference);
+
+
+    }
+
+    private void bindPreferenceChangeListener(Preference preference){
+        preference.setOnPreferenceChangeListener(this);
+
+        //trigger the onPreferenceChange
+        onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+        preference.setSummary(stringValue);
+        return true;
     }
 }
