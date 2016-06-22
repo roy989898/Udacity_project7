8508b7dbd8c83891286a73ea8ee409cfd7a62459
roy989898 <roy989898@gmail.com>
2016-06-22 19:31:03 +0800

    feat:add the back button to the Setting Activity

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/SettingActivity.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/SettingActivity.java
@@ -1,13 +1,44 @@
 package pom.poly.com.tabatatimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+
+        //set the title of the action bar
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.title_settingActivity);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+       /* switch (item.getItemId()){
+
+        }
+        return super.onOptionsItemSelected(item);*/
+        return super.onOptionsItemSelected(item);
     }
 }
