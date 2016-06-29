78ee70f1b3258291b8337779cda768cbd502cb7c
roy989898 <roy989898@gmail.com>
2016-06-29 15:26:25 +0800

    feat:when click save,will create a new use on the firebase data base

---
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Firebase/User.java
@@ -0,0 +1,25 @@
+package pom.poly.com.tabatatimer.Firebase;
+
+/**
+ * Created by User on 29/6/2016.
+ */
+public class User {
+    public int likeNumber;
+    public String userID;
+    public String email;
+    public String userName;
+    public String profileLink;
+    public long totaltime;
+
+    public User(String email, int likeNumber, String profileLink, long totaltime, String userID, String userName) {
+        this.email = email;
+        this.likeNumber = likeNumber;
+        this.profileLink = profileLink;
+        this.totaltime = totaltime;
+        this.userID = userID;
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+}
--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/FinishDialogFragment.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/FinishDialogFragment.java
@@ -2,10 +2,12 @@ package pom.poly.com.tabatatimer.Fragment;
 
 
 import android.app.Dialog;
-import android.graphics.Typeface;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +15,18 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import pom.poly.com.tabatatimer.ContentProvider.Eventinf;
+import pom.poly.com.tabatatimer.Firebase.User;
 import pom.poly.com.tabatatimer.R;
 
 /**
@@ -26,6 +36,7 @@ public class FinishDialogFragment extends DialogFragment {
 
 
     private final static String BUNDLE_KEY = "KEY_GETEVENTIF";
+    private static final String TAG = "in FinishDialogFragment";
     @BindView(R.id.btDialogCancel)
     Button btDialogCancel;
     @BindView(R.id.btDialogSave)
@@ -36,6 +47,14 @@ public class FinishDialogFragment extends DialogFragment {
     TextView tvTotalTime;
 
     private Eventinf eventif;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private User user;
+    private String email;
+    private String useID;
+    private DatabaseReference idREF;
+    private String profilePURL;
+    private String name;
 
     public FinishDialogFragment() {
     }
@@ -77,6 +96,11 @@ public class FinishDialogFragment extends DialogFragment {
 
         tvfinishTime.setText(eventif.getFinish_time());
         tvTotalTime.setText(eventif.getTotalTime() + "");
+
+
+        //for save to the firebase use
+        profilePURL = getContext().getSharedPreferences(getString(R.string.name_sharepreference), Context.MODE_PRIVATE).getString(getString(R.string.SharePreferenceDownloadLinkKey), "");
+        name= PreferenceManager.getDefaultSharedPreferences(getContext()).getString(getString(R.string.preference_name_key),getString(R.string.preference_name_defaultvalue));
     }
 
     @OnClick({R.id.btDialogCancel, R.id.btDialogSave})
@@ -87,9 +111,59 @@ public class FinishDialogFragment extends DialogFragment {
                 break;
             case R.id.btDialogSave:
                 eventif.save();
+                saveANDupdateFireBase();
                 dismiss();
                 break;
         }
+    }
+
+    private void saveANDupdateFireBase() {
+        /*first chec is the use appear on the firebase
+        * if no
+        * hust save to the Firebase database
+        * if yes
+        * update children
+        * update
+        * 1name
+        * 2profile picturle link
+        * 3time*/
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        //get the email first
+         email = mAuth.getCurrentUser().getEmail();
+        useID=mAuth.getCurrentUser().getUid();
+        
+        //get the reference at the login user email
+         idREF = mDatabase.child("Users").child(useID);
+        //read it one times by listener
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+
+                if (user == null) {
+                    //create a new one
+
+                    String id = mAuth.getCurrentUser().getProviderId();
+
+                    User newuser=new User(email,0,profilePURL,eventif.getTotalTime(),id,name);
+                    idREF.setValue(newuser);
+                } else {
+                    //update  exiting one//TODO
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadsingle use by email:onCancelled", databaseError.toException());
+            }
+        };
+
+        idREF.addListenerForSingleValueEvent(listener);
+
     }
 
     @Override
