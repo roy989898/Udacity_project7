a398b6d5e22a219712225ef1d39f7616b0e36241
roy989898 <roy989898@gmail.com>
2016-07-02 18:09:46 +0800

    feat:add a like button to the Ranking page

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/ContentProvider/RankingCursorAdapter.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/ContentProvider/RankingCursorAdapter.java
@@ -2,22 +2,32 @@ package pom.poly.com.tabatatimer.ContentProvider;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import pom.poly.com.tabatatimer.Firebase.User;
 import pom.poly.com.tabatatimer.R;
 
 /**
  * Created by User on 1/7/2016.
  */
 public class RankingCursorAdapter extends CursorAdapter {
+
     public RankingCursorAdapter(Context context, Cursor c) {
         super(context, c);
     }
@@ -32,17 +42,58 @@ public class RankingCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
         final ViewHolder vh = (ViewHolder) view.getTag();
         String name = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_NAME));
         String profileLink = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_PROFILE_LINK));
         long totalTime = cursor.getLong(cursor.getColumnIndex(Contract.UserEntry.COLUMN_TOTAL_TIME));
+        final int likeNumber = cursor.getInt(cursor.getColumnIndex(Contract.UserEntry.COLUMN_LIKENUMBER));
+        String userID = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_USERID));
+        vh.userID=userID;
+        Log.i("userID", userID);
         vh.tvName.setText(name);
         vh.txTotalTime.setText(totalTime + "");
+        vh.txLikeNumber.setText(likeNumber + "");
 
 //        load the profileLink picture
         vh.profilePicture.setImageURI(profileLink);
 
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO like numer +1 at Firebase
+                DatabaseReference baseRef = FirebaseDatabase.getInstance().getReference();
+
+
+                DatabaseReference userREF = baseRef.child("Users").child(vh.userID);
+                addoneToFireBaseIteam(userREF);
+            }
+        };
+        vh.imageButton.setOnClickListener(clickListener);
+
+    }
+
+    private void addoneToFireBaseIteam(DatabaseReference userRef) {
+        userRef.runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                User user = mutableData.getValue(User.class);
+                if (user == null) {
+                    return Transaction.success(mutableData);
+                }
+                user.likeNumber = user.likeNumber + 1;
+
+                mutableData.setValue(user);
+
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d("addoneToFireBaseIteam", "postTransaction:onComplete:" + databaseError);
+            }
+        });
     }
 
 
@@ -51,8 +102,15 @@ public class RankingCursorAdapter extends CursorAdapter {
         TextView tvName;
         @BindView(R.id.profile_picture)
         SimpleDraweeView profilePicture;
+        @BindView(R.id.imageButton)
+        ImageButton imageButton;
+        @BindView(R.id.txLikeNumber)
+        TextView txLikeNumber;
         @BindView(R.id.txTotalTime)
         TextView txTotalTime;
+
+        public String userID;
+
 
         ViewHolder(View view) {
             ButterKnife.bind(this, view);
