330763a64d8c62b9bca32b3861adb6a2a8355655
roy989898 <roy989898@gmail.com>
2016-07-03 09:41:52 +0800

    fix:fix the password vaild function

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/LoginActivity.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/LoginActivity.java
@@ -106,7 +106,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private boolean isaValidPassw(String password) {
 
-        String re1 = "[a-zA-Z0-6@~!]{6,}";
+//        String re1 = "[a-zA-Z0-6@~!]{6,}";
+        String re1 = "[a-zA-Z0-9]{6,}";
         Pattern p = Pattern.compile(re1, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
         Matcher m = p.matcher(password);
 
