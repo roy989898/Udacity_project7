c167720733d1b404070bb365434d2d71e7edabf6
roy989898 <roy989898@gmail.com>
2016-07-06 08:53:37 +0800

    style:improve the code style in  RankingCursorAdapter.java

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/ContentProvider/RankingCursorAdapter.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/ContentProvider/RankingCursorAdapter.java
@@ -63,34 +63,35 @@ public class RankingCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, final Cursor cursor) {
         final ViewHolder vh = (ViewHolder) view.getTag();
+
+        //get the data from the cursor
         String name = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_NAME));
         String profileLink = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_PROFILE_LINK));
         long totalTime = cursor.getLong(cursor.getColumnIndex(Contract.UserEntry.COLUMN_TOTAL_TIME));
         final int likeNumber = cursor.getInt(cursor.getColumnIndex(Contract.UserEntry.COLUMN_LIKENUMBER));
         String userID = cursor.getString(cursor.getColumnIndex(Contract.UserEntry.COLUMN_USERID));
+
+        //format the total time hour:minutes:seconds
+        long[] timeArray = getDurationBreakdown(totalTime);
+
+        //put data into view holder
         vh.userID = userID;
         Log.i("userID", userID);
         vh.tvName.setText(name);
-        //format the total time hourd:minutes:seconds
-        long[] timeArray = getDurationBreakdown(totalTime);
         vh.tvHours.setText(timeArray[0] + "");
         vh.tvMinutes.setText(timeArray[1] + "");
         vh.tvSeconds.setText(timeArray[2] + "");
-
         vh.txLikeNumber.setText(likeNumber + "");
         vh.profilePicture.setImageURI(profileLink);
 
-
-//        load the profileLink picture
 
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatabaseReference baseRef = FirebaseDatabase.getInstance().getReference();
-
-
                 DatabaseReference userREF = baseRef.child("Users").child(vh.userID);
+                
                 addoneToFireBaseIteam(userREF);
             }
         };
