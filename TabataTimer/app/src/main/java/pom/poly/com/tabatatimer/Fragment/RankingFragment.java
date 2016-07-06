224a69697e017d050846c61cdafa585b41c1edb0
roy989898 <roy989898@gmail.com>
2016-07-06 15:35:15 +0800

    feat:change the message input method

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/RankingFragment.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/Fragment/RankingFragment.java
@@ -1,40 +1,57 @@
 package pom.poly.com.tabatatimer.Fragment;
 
 
+import android.animation.Animator;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pom.poly.com.tabatatimer.ContentProvider.Contract;
 import pom.poly.com.tabatatimer.ContentProvider.RankingCursorAdapter;
+import pom.poly.com.tabatatimer.Firebase.Message;
 import pom.poly.com.tabatatimer.R;
+import pom.poly.com.tabatatimer.Utility.Utility;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RankingFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>,RankingCursorAdapter.MessageHandler {
+public class RankingFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, RankingCursorAdapter.MessageHandler {
 
     final private int MY_LODER_ID = 101;
     @BindView(R.id.lv_ranking)
     ListView lvRanking;
     @BindView(R.id.tvEmptyMessageView)
     TextView tvEmptyMessageView;
+    @BindView(R.id.editText)
+    EditText editText;
+    @BindView(R.id.btSend)
+    Button btSend;
+    @BindView(R.id.btCancel)
+    Button btCancel;
+    @BindView(R.id.cardViewmessageBox)
+    CardView cardViewmessageBox;
 
 
     private DatabaseReference ref;
@@ -98,8 +115,75 @@ public class RankingFragment extends Fragment implements LoaderManager.LoaderCal
     }
 
     @Override
-    public void send(String uid) {
+    public void send(final String uid) {
+        fadein();
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long datetime = Calendar.getInstance().getTimeInMillis();
+                String fromID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                Message message = new Message(datetime, fromID, editText.getText().toString(), uid);
+                Utility.sendMessage(message);
 
+                fadeout();
+            }
+        });
+
+    }
+
+    @OnClick({R.id.btCancel})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btCancel:
+                fadeout();
+                break;
+        }
+    }
+
+    private void fadein() {
+        int mShortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+        cardViewmessageBox.setAlpha(0f);
+        cardViewmessageBox.setVisibility(View.VISIBLE);
+        cardViewmessageBox.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        editText.requestFocus();
+
+    }
+
+    private void fadeout() {
+        int mShortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+        //clear the message input
+        editText.getText().clear();
+        cardViewmessageBox.setAlpha(1f);
+        cardViewmessageBox.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        cardViewmessageBox.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                });
 
     }
 }
