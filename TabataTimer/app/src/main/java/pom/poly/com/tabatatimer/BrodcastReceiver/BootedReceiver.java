b5620aeb37b8ec6b7f3654d3e7daa813f273be50
roy989898 <roy989898@gmail.com>
2016-07-10 20:08:30 +0800

    featcan so the message of a notification on a specifice time every day,even after boot

--- TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/BrodcastReceiver/BootedReceiver.java
+++ TabataTimer/app/src/main/java/pom/poly/com/tabatatimer/BrodcastReceiver/BootedReceiver.java
@@ -1,12 +1,17 @@
 package pom.poly.com.tabatatimer.BrodcastReceiver;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
+import pom.poly.com.tabatatimer.MainActivity;
 import pom.poly.com.tabatatimer.R;
 import pom.poly.com.tabatatimer.Utility.Utility;
 
@@ -18,10 +23,11 @@ public class BootedReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Log.d("NotificationReceiver", "booted receive");
+        Log.d("NotificationReceiverBoot", "booted receive");
 
         SharedPreferences defaultpreference = PreferenceManager.getDefaultSharedPreferences(context);
         boolean switchOnOff = defaultpreference.getBoolean(context.getString(R.string.preference_nof_key), false);
+        Log.d("NotificationReceiverBoot", switchOnOff+"");
         if(switchOnOff){
 //                    on
             String timeString = defaultpreference.getString(context.getString(R.string.preference_timePick_key), context.getString(R.string.preference_timePick_defaultvalue));
@@ -29,4 +35,6 @@ public class BootedReceiver extends BroadcastReceiver {
 
         }
     }
+
+
 }
